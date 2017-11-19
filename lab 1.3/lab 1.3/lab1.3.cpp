e38f91cbd58e4966cd7802bcf1423ccbc2b484df
executioner787 <kominko.vitalik@gmail.com>
2017-11-19 14:50:03 +0200

    Add files via upload

---
+++ lab 1.3/lab 1.3/lab1.3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>//we included iostream
+#include <iomanip>//we included iomanip
+#include <string>//we included string
+//using namespace std;
+int main()
+{
+	int v = 10800; // 3*60*60
+	int z = 3 * 60;// This is a variable to hold the value of 3 minutes in sseconds
+	int zz = 5 * 60;// This is a variable to hold the value of 5 minutes in seconds
+	float pi = 3.141526; //This is the value of pi
+	std::cout << "result: " << v << std::endl;
+	// here we should print the v value
+	system("pause");
+	return 0;
+}
